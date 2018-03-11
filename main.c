3279162c64d4f055b2446142f45582deabe62cd8
Kawaharasouta <kawahara6514@gmail.com>
2018-03-11 17:53:08 +0900

    add arp

--- main.c
+++ main.c
@@ -14,6 +14,9 @@
 
 #define SIZE 1024
 
+u_int16_t swap_16byte(u_int16_t num);
+void analyze_arp(u_char *packet, int size);
+
 int main(int argc, char* argv[]){
   if (argc  != 2) {
 		fprintf(stderr, "useage ./main [filename]\n");
@@ -54,6 +57,8 @@ int main(int argc, char* argv[]){
 		packet += sizeof(struct ether_header);
 		size -= sizeof(struct ether_header);
 
+		printf("Ethernet\n");
+
 		printf("dest: %02x:%02x:%02x:%02x:%02x:%02x\n",eth->ether_dhost[0], eth->ether_dhost[1], eth->ether_dhost[2],eth->ether_dhost[3],eth->ether_dhost[4],eth->ether_dhost[5]);
 		printf("source: %02x:%02x:%02x:%02x:%02x:%02x\n",eth->ether_shost[0], eth->ether_shost[1], eth->ether_shost[2],eth->ether_shost[3],eth->ether_shost[4],eth->ether_shost[5]);
 		printf("ether type: %04x\n", eth->ether_type);
@@ -61,14 +66,16 @@ int main(int argc, char* argv[]){
 		//u_int16_t head, tail;
 		//head = (eth->ether_type & 0xff00) >> 8;
 		//tail = (eth->ether_type & 0x00ff) << 8;
-		u_int16_t type_num = ((eth->ether_type & 0xff00) >> 8) + ((eth->ether_type & 0x00ff) << 8);
+		//u_int16_t type_num = ((eth->ether_type & 0xff00) >> 8) + ((eth->ether_type & 0x00ff) << 8);
+		u_int16_t type_num = swap_16byte(eth->ether_type);
 
 		printf("ether type true: %04x\n", type_num);
 
 		if (type_num == ETHERTYPE_ARP){
 			printf("arp\n");
+			analyze_arp(packet, size);
 		}
-		if (type_num == ETHERTYPE_IP) {
+		else if (type_num == ETHERTYPE_IP) {
 			printf("ip\n");
 		}
 
@@ -81,4 +88,56 @@ int main(int argc, char* argv[]){
 
 	//fclose(fp);
 	pcap_close(p);
+}
+
+
+u_int16_t swap_16byte(u_int16_t num){
+	return (u_int16_t)((num & 0xff00) >> 8) + ((num & 0x00ff) << 8);
+}
+
+void analyze_arp(u_char *packet, int size){
+	struct ether_arp *eth_arp;
+	eth_arp = (struct ether_arp *) packet;
+
+	//increment
+	packet += sizeof(struct ether_arp);
+	size -= sizeof(struct ether_arp);
+
+	printf("ARP\n");
+
+	u_int16_t hrd = swap_16byte(eth_arp->ea_hdr.ar_hrd);
+	printf("hard type: %x\n", hrd);
+
+	u_int16_t proto = swap_16byte(eth_arp->ea_hdr.ar_pro);
+	printf("proto type: %x\n", proto);
+
+	u_int16_t op = swap_16byte(eth_arp->ea_hdr.ar_op);
+	printf("arp operation: %x\n", op);
+
+//	if (op == ARPOP_REQUEST){
+//
+//	}
+//	else if (op == ARPOP_REPLY){
+//
+//	}
+
+	switch (op){
+		case ARPOP_REQUEST:
+			printf("arp_request\n");i
+			arp_request()
+			break;
+		case ARPOP_REPLY:
+			printf("arp_reply");
+			arp_reply();
+			break;
+		default:
+			break;
+	}
+}
+
+void arp_request(){
+
+}
+void arp_reply(){
+
 }
